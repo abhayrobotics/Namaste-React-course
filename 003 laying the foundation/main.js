bcf04cfc553434a4e055b1311ebe0c7f15a2d067
abhaygupt <abhayrobotics1908@gmail.com>
2023-09-06 11:04:34 +0530

    jsx start

--- 003 laying the foundation/main.js
+++ 003 laying the foundation/main.js
@@ -1,2 +1,8 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
+
+const heading = React.createElement("h1",{"class":"heading1"},"Namaste React!")
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading)
