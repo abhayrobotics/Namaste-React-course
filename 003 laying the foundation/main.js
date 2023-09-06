c7c93aac270d0da9fb23e210143fb05d4b5ca6b5
abhaygupt <abhayrobotics1908@gmail.com>
2023-09-06 21:00:07 +0530

    functional component

--- 003 laying the foundation/main.js
+++ 003 laying the foundation/main.js
@@ -1,8 +1,37 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1",{"class":"heading1"},"Namaste React!")
+
+// ? React Element
+const jsxHeading =
+    (<h1 className="heading">
+        Namaste React using JSX
+    </h1>)
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading)
+// root.render(jsxHeading)
+
+// ? React Component
+// 1.Functional Based component ( new)
+// 2. class  Based component ( old )
+
+// ? Functional Based component
+
+const Title = () => {
+    return <h1 className="title" >Title - Namaste</h1>
+};
+
+// ? Function composition- functional ocmponent inside another
+const Header = () => {
+
+        return <div className="header">
+                    <Title />
+                    <h1 className="heading" >Hello Namaste 1</h1>
+                    <h1 className="heading" >Hello Namaste 2 </h1>
+                    <h1 className="heading" >Hello Namaste 3 </h1>
+                </div>
+
+};
+root.render(<Header />)
