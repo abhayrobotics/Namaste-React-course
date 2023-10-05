b281ddf9d5e7f0dfac07da1c59bf7a551e7365a3
abhaygupt <abhayrobotics1908@gmail.com>
2023-10-05 08:04:23 +0530

    clas based component

--- 004 Food App/src/components/About.js
+++ 004 Food App/src/components/About.js
@@ -1,13 +1,16 @@
 
+import User from "./User";
+import UserClass from './UserClass'
 
-const About = ()=>{
+const About = () => {
 
-    return(
+    return (
         <div>
-           
-           
+
             <h2>About Us</h2>
-            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas ipsum nostrum blanditiis qui porro corporis similique praesentium. Totam, inventore sapiente! Qui facere laboriosam ad distinctio, explicabo illum vel placeat omnis incidunt quam optio corporis architecto dolorum minima quasi ut sit libero amet nemo modi velit, quis nesciunt? Alias, repellat id?</h4>
+            <User name={"Abhay Kumar Gupta "} location={" Dhanbad"} />
+
+            <UserClass />
         </div>
     )
 }
---
+++ 004 Food App/src/components/UserClass.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+class UserClass extends React.Component{
+
+    render(){
+
+        return (
+            <div className="user-card">
+                <h3>Name: Abhay kumar</h3>
+                <h3>Location: Dhanbad</h3>
+                <h3>Github: abhayrobotics</h3>
+            </div>
+        )
+     
+    }
+
+}
+export default UserClass;
