6ba210fb5fc00364111e1dbcedcdf6fbd2386c8b
abhaygupt <abhayrobotics1908@gmail.com>
2023-10-14 06:52:33 +0530

    custom hook of restauranant menu fetch data created

--- 004 Food App/src/components/RestaurantMenu.js
+++ 004 Food App/src/components/RestaurantMenu.js
@@ -1,48 +1,15 @@
-import { useEffect, useState } from "react";
 import ShimmerBody from "./ShimmerBody";
-import { Menu_URL } from "../utils/constant";
-
 import { useParams } from "react-router-dom";
+// Custom hook
+import useRestaurantInfo from "../utils/useRestaurantInfo";
 
 const RestaurantMenu = () => {
 
-    const [resMenu, setResMenu] = useState(null)
-    const [resFood, setFoodInfo] = useState(null)
-
     const { resid } = useParams()
-    console.log(resid)
+    
+    // ? Custom Hook
+    const [resMenu, resFood] = useRestaurantInfo(resid)
 
-    //? fetch restaurant menu after poage loads
-    useEffect(() => {
-        console.log("useEffect called")
-        fetchMenu();
-        //? after data fetch  destructuring
-
-
-    }, [])
-    console.log(useState())
-
-    const fetchMenu = async () => {
-
-        const response1 = await fetch(Menu_URL + resid)
-        const json = await response1.json()
-       
-
-
-        const resData = json?.data?.cards[0]?.card?.card?.info
-
-        // const resFoodData = json?.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.categories[0].itemCards
-        const resFoodData = json?.data?.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.itemCards
-        setResMenu(resData)
-        setFoodInfo(resFoodData)
-        console.log(resMenu)
-
-    }
-
-
-
-    // const {} = resMenu?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card?.itemCards;
-    // console.log(name,avgRating,areaName,city,cuisines)
     if (resMenu === null) {
         return <ShimmerBody />
     }
---
+++ 004 Food App/src/utils/useRestaurantInfo.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { Menu_URL } from "./constant";
+
+const useRestaurantInfo= (resid)=>{
+
+    const [resMenu,setResMenu]= useState(null);
+    const [resFood,setFoodInfo] = useState(null)
+
+    useEffect(()=>{
+        fetchData();
+
+    },[])
+
+    const fetchData= async ()=>{
+
+        const response = await fetch(Menu_URL+resid)
+        const json=  await response.json();
+
+        const resData = json?.data?.cards[0]?.card?.card?.info
+
+        const resFoodData = json?.data?.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.itemCards
+        setResMenu(resData)
+        setFoodInfo(resFoodData)
+
+    }
+  
+    return [resMenu,resFood]
+}
+
+export default useRestaurantInfo;
