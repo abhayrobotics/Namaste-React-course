3df0478d0ae99498f973b22a95c2c795ee1cdcd0
abhaygupt <abhayrobotics1908@gmail.com>
2023-12-17 13:32:32 +0530

    swiggy menu cards

--- 004 Food App/src/components/RestaurantMenu.js
+++ 004 Food App/src/components/RestaurantMenu.js
@@ -14,13 +14,13 @@ const RestaurantMenu = () => {
         return <ShimmerBody />
     }
     return (
-        <div className="flex justify-center">
-            <div className="flex self-center jutify-center w-6/12 border-2  border-black">
+        <div className="flex justify-center my-2">
+            <div className="flex self-center jutify-center w-7/12 ">
 
-                <div className="items-center  text p-2" >
+                <div className="items-center p-2" >
 
 
-                    <h3 className="font-semibold text-xl">{resMenu.name}</h3>
+                    <h3 className="font-semibold text-2xl">{resMenu.name}</h3>
 
                     <p>{resMenu.cuisines.join(", ")}</p>
                     <p>{resMenu.areaName + ", " + resMenu.city}</p>
@@ -32,44 +32,47 @@ const RestaurantMenu = () => {
                     {/* showing each category for indivisual restuarant */}
 
                     {resFood.map((foodCategory) => {
-                            if (foodCategory.card.card["@type"] == "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory") {
-                                console.log(foodCategory)
-                                return (
-                                    <div className="shadow-md my-3 ">
-                                        <div className="flex justify-between">
-                                        <div className="font-semibold ">{foodCategory.card.card.title} ({foodCategory.card.card.itemCards.length})</div>
-                                        <div>^</div>
+                        if (foodCategory.card.card["@type"] == "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory") {
+                            console.log(foodCategory)
+                            return (
+                                <div className="shadow-md my-3 bg-slate-100 px-2 rounded-lg">
+                                    <div className="flex justify-between cursor-pointer">
+                                        <div className="font-extrabold text-lg text-gray-800 ">{foodCategory.card.card.title} ({foodCategory.card.card.itemCards.length})</div>
+                                        <div className="font-extrabold text-xl">^</div>
 
-                                        </div>
-
-                                    {/* showing each Food in that category*/}
-                                        <ul className=" flex flex-col  justify-center items-center">
-                                            {foodCategory.card.card.itemCards.map((item) => {
-                                                return (
-                                                    <div key={item?.card?.info?.id}>
-                                                        <li className=" flex  place-content-between min-w-[650px]  my-2 ">
-                                                            <div className="text ">
-                                                                <h4>{item?.card?.info?.name}  </h4>
-                                                                <h4>Rs. {item?.card?.info?.price / 100 || item?.card?.info?.defaultPrice / 100}</h4>
-                                                                <p className="text-slate-500">{item?.card?.info?.description}</p>
-
-                                                            </div>
-                                                            <img className="  w-32 rounded-md object-cover" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/" + item.card.info.imageId} alt="" />
-
-                                                        </li>
-                                                        <hr />
-                                                    </div>
-                                                )
-                                            })
-                                            }
-                                        </ul>
-                                        <hr />
                                     </div>
 
-                                )
-                            }
+                                    {/* showing each Food in that category*/}
+                                    <ul className=" flex flex-col  justify-center  ">
+                                        {foodCategory.card.card.itemCards.map((item) => {
+                                            return (
+                                                <div key={item?.card?.info?.id}>
+                                                    <li className=" flex   justify-between   my-2 ">
+                                                        <div className="text ">
+                                                            <h4 className="text-md text-gray-800 font-bold">{item?.card?.info?.name}  </h4>
+                                                            <h4>Rs. {item?.card?.info?.price / 100 || item?.card?.info?.defaultPrice / 100}</h4>
+                                                            <p className="text-slate-500 text-sm">{item?.card?.info?.description}</p>
 
-                        })}
+                                                        </div>
+                                                        <div className="relative">
+
+                                                        <img className="  w-32 rounded-md object-cover " src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/" + item.card.info.imageId} alt="" />
+                                                        <span className="absolute  bottom-0 right-0 px-2 py-1 text-sm rounded text-green-500 bg-white  border-1 border-black shadow-lg cursor-pointer font-bold">Add+</span>
+                                                        </div>
+                                                    </li>
+                                                    <hr />
+                                                </div>
+                                            )
+                                        })
+                                        }
+                                    </ul>
+                                    <hr />
+                                </div>
+
+                            )
+                        }
+
+                    })}
                 </div>
             </div>
         </div>
