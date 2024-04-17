dfdc79cd6b7d0b2f7b2f53248077cead66f8b86a
Abhay kumar <abhayrobotics1908@gmail.com>
2024-04-17 23:30:38 +0530

    Functional component

--- 005 Practice App/2 Practice app setup/app.js
+++ 005 Practice App/2 Practice app setup/app.js
@@ -1,13 +1,30 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //* Method-1
 // const heading =React.createElement('h1',{},"Hello React using react installation");
-// const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(heading)
 
 // *Method-2
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const jsxHeading = <h1>Hello react using JSX syntax</h1>
-root.render(jsxHeading)
+// const jsxHeading = <h1>Hello react using JSX syntax</h1>
+// root.render(jsxHeading)
+
+// 3. functional component
+
+const Header = () => {
+  return <h2> I am Header Component</h2>;
+};
+
+const Body = () => {
+  return (
+    <>
+      <Header />
+      <h2>I am Body Component</h2>
+    </>
+  );
+};
+
+root.render(<Body />)
