85cbc0ccfc734ef8432a68bdcacf2b9d438b569c
Abhay kumar <abhayrobotics1908@gmail.com>
2024-04-17 23:50:48 +0530

    main page working after api change

--- 004 Food App/src/components/Body.js
+++ 004 Food App/src/components/Body.js
@@ -36,10 +36,11 @@ const Body = () => {
             const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
 
             const json = await data.json();
+            console.log(json.data );
             // console.log(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants );
             // ? optional chaining
-            const filterApiData = json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-            // console.log( filterApiData)
+            const filterApiData = json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+            console.log( filterApiData)
 
             setListofRestuarant(filterApiData)
             setFilterRestaurant(filterApiData)
