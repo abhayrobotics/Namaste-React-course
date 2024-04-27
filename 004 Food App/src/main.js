e3cdd2a2787bf0f527a046f3c8eaa77e9447ed72
Abhay kumar <abhayrobotics1908@gmail.com>
2024-04-27 20:50:31 +0530

    new cart component

--- 004 Food App/src/main.js
+++ 004 Food App/src/main.js
@@ -8,6 +8,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
+import Cart from "./components/Cart";
 // import Grocery from "./components/Grocery";
 
 // redux
@@ -60,6 +61,10 @@ const newRouter = createBrowserRouter([
         path: "/restaurant/:resid",
         element: <RestaurantMenu />,
       },
+      {
+        path:"/cart",
+        element:<Cart />
+      }
     ],
   },
 ]);
