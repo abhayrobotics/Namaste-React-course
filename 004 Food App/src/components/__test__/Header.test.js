b546caed9f874057083d1c27394b28111f038a19
Abhay kumar <abhayrobotics1908@gmail.com>
2024-05-02 13:26:51 +0530

    header yest case complete

--- 004 Food App/src/components/__test__/Header.test.js
+++ 004 Food App/src/components/__test__/Header.test.js
@@ -1,6 +1,6 @@
 import { Provider } from "react-redux";
 import Header from "../Header";
-import { render,screen } from "@testing-library/react";
+import { fireEvent, render,screen } from "@testing-library/react";
 import appStore from "../../utils/appStore";
 import { BrowserRouter } from "react-router-dom";
 import "@testing-library/jest-dom"
@@ -16,7 +16,7 @@ it("shoud load Header component with login button", () => {
   );
 
   // query
-//   const login = screen.getByText("Login")
+  const login = screen.getByText("Login")
   
   // assertion
   expect(login).toBeInTheDocument();
@@ -39,3 +39,25 @@ it("shoud load Header component 6 header items", () => {
   // assertion
   expect(login.length).toBe(6);
 });
+
+
+it("shoud change login button to log out on clik", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  // query
+  const login = screen.getByText("Login")
+
+//   fireevent
+  fireEvent.click(login)
+
+  const logout =screen.getByText("Logout")
+  // assertion
+  expect(logout).toBeInTheDocument();
+});
+
